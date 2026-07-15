e22dea32e577cb43e55011e4c14ecdb2c9f9dbea
Dew <no@one.com>
2026-07-15 14:26:17 -0500

    Moved where images get downloaded to into their own folder. Now check if folder executable is in has write permissions before downloading images.

--- SteamGameLauncher/SteamGameLauncher.cs
+++ SteamGameLauncher/SteamGameLauncher.cs
@@ -6,7 +6,7 @@ namespace PhysicalSteamGameLauncher
 {
     public partial class SteamGameLauncher : Form
     {
-        private string path = (string)AppContext.BaseDirectory;
+        private string path;
 
         public SteamGameLauncher()
         {
@@ -15,9 +15,10 @@ namespace PhysicalSteamGameLauncher
 
         private void SteamGameLaucnher_Load(object sender, EventArgs e)
         {
-            int windowWidth = 600;
+            int windowWidth = 300;
             int windowHeight = 450;
             PictureBox pictureBox;
+            string imagesPath = path + "\\Images";
             List<string> gamesList = File.ReadAllLines($"{path}\\games.txt").ToList();
             for (int i = gamesList.Count - 1; i >= 0; i--)
             {
@@ -26,22 +27,37 @@ namespace PhysicalSteamGameLauncher
                     gamesList.RemoveAt(i);
                 }
             }
+
+            if (!Directory.Exists(imagesPath) && IsDirectoryWritable(path))
+            {
+                Directory.CreateDirectory(imagesPath);
+            }
+
             foreach (var game in gamesList)
             {
                 pictureBox = new PictureBox();
-                if (!File.Exists($"{path}\\{game}.jpg"))
+                if (IsDirectoryWritable(path))
                 {
-                    using (WebClient client = new WebClient())
+                    if (!File.Exists($"{imagesPath}\\{game}.jpg"))
                     {
-                        client.DownloadFile(new Uri($"https://steamcdn-a.akamaihd.net/steam/apps/{game}/library_600x900_2x.jpg"), $"{path}\\{game}.jpg");
-                    };
+                        try
+                        {
+                            using (WebClient client = new WebClient())
+                            {
+                                client.DownloadFile(new Uri($"https://steamcdn-a.akamaihd.net/steam/apps/{game}/library_600x900_2x.jpg"), $"{imagesPath}\\{game}.jpg");
+                            };
+                        }
+                        catch
+                        {
+                            MessageBox.Show($"An image for the game with the app Id of {game} was not able to be downloaded. Please download the 600x900 separately and add it alongside the other images within the Images folder.", "Game ID Graphic Not Found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
                 }
 
-                pictureBox.ImageLocation = $"{path}\\{game}.jpg";
-                pictureBox.Location = new Point(gamesList.IndexOf(game) * 300, 0);
+                pictureBox.ImageLocation = $"{imagesPath}\\{game}.jpg";
+                pictureBox.Location = new Point(gamesList.IndexOf(game) * windowWidth, 0);
                 pictureBox.Name = $"pictureBox{game}";
-                pictureBox.Size = new Size(300, 450);
-                pictureBox.BackColor = Color.Black;
+                pictureBox.Size = new Size(windowWidth, windowHeight);
                 pictureBox.MouseEnter += PictureBox_MouseEnter;
                 pictureBox.MouseLeave += PictureBox_MouseLeave;
                 pictureBox.Click += PictureBox_Click;
@@ -50,15 +66,14 @@ namespace PhysicalSteamGameLauncher
                 this.Controls.Add(pictureBox);
             }
 
-            if (gamesList.Count > 2)
-            {
-                windowWidth = 300 * gamesList.Count;
-            }
-
             if (gamesList.Count > 4)
             {
                 windowWidth = 1350;
                 windowHeight = 500;
+            }
+            else
+            {
+                windowWidth = 300 * gamesList.Count;
             }
 
             ClientSize = new Size(windowWidth, windowHeight);
@@ -66,18 +81,18 @@ namespace PhysicalSteamGameLauncher
 
         private void PictureBox_MouseEnter(object sender, EventArgs e)
         {
-            double zoomFactor = 1.05;
             PictureBox picture = (PictureBox)sender;
             Image image = picture.Image;
             if (image != null)
             {
                 using (Graphics g = Graphics.FromImage(image))
                 {
-                    Pen pen = new Pen(Color.FromArgb(90, 0, 0, 0), 6000);
+                    Pen pen = new Pen(Color.FromArgb(100, 0, 0, 0), 6000);
                     g.DrawLine(pen, -1, -1, 6000, 9000);
                     g.Save();
                 }
             }
+
             picture.Image = image;
         }
 
@@ -91,20 +106,34 @@ namespace PhysicalSteamGameLauncher
         {
             PictureBox picture = (PictureBox)sender;
             string gameId = picture.Name.Replace("pictureBox", string.Empty);
-            if (File.Exists("C:\\Program Files (x86)\\Steam\\steamapps\\libraryfolders.vdf") && !File.ReadAllText("C:\\Program Files (x86)\\Steam\\steamapps\\libraryfolders.vdf").Contains($"\"{gameId}\""))
+            string steamUrl = $"steam://rungameid/{gameId}";
+            Process.Start(new ProcessStartInfo
             {
-                MessageBox.Show("It looks like this game is not installed on your device. Either install the game through Steam or restore the game backup to continue.", "Install Game", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); 
-            }
-            else
-            {
-                string steamUrl = $"steam://rungameid/{gameId}";
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = steamUrl,
-                    UseShellExecute = true
-                });
+                FileName = steamUrl,
+                UseShellExecute = true
+            });
 
-                this.Close();
+            this.Close();
+        }
+
+        private bool IsDirectoryWritable(string dirPath, bool throwIfFails = false)
+        {
+            try
+            {
+                using (FileStream fs = File.Create(Path.Combine(dirPath, Path.GetRandomFileName()), 1, FileOptions.DeleteOnClose))
+                { }
+                return true;
+            }
+            catch
+            {
+                if (throwIfFails)
+                {
+                    throw;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
     }
